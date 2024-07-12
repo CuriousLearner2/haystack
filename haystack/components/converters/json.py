c97ff9ffb8d0ea16fe51b45984dc50bf1c6e4a02
kanenorman <kanenorman1996@gmail.com>
2024-07-12 10:27:33 -0500

    Enable add metadata for JSONConverter

--- haystack/components/converters/json.py
+++ haystack/components/converters/json.py
@@ -36,7 +36,7 @@ class JSONToDocument:
         jq_schema: str,
         content_key: Optional[str] = None,
         is_content_key_jq_parsable: bool = False,
-        is_json_lines: bool = False,
+        additional_meta_fields: Optional[List[str]] = None,
     ):
         """
         Create an JSONToDocument component.
@@ -50,17 +50,19 @@ class JSONToDocument:
         :param is_content_key_jq_parsable:
             A flag to determine if content_key is parsable by jq or not.
             If True, content_key is treated as a jq schema and compiled accordingly.
-            If False or if content_key is None, content_key is used as a simple string. Default is False.
-        :param is_json_lines:
-            Boolean flag to indicate whether the input is in JSON Lines format.
-
+            If False or if content_key is None, content_key is used as a simple string.
+            Default is False.
+        :param additional_meta_fields:
+            A list of additional fields to extract from the JSON and add to the metadata.
+            Takes precedence over the metadata specified in the `meta` parameter of the `run` method.
+            Default is None.
         """
         jq_import.check()
 
         self._jq_schema = jq.compile(jq_schema)
         self._content_key = content_key
         self._is_content_key_jq_parsable = is_content_key_jq_parsable
-        self._is_json_lines = is_json_lines
+        self._additional_meta = additional_meta_fields
 
     @component.output_types(documents=List[Document])
     def run(
@@ -95,26 +97,19 @@ class JSONToDocument:
                 logger.warning("Could not read '{source}'. Skipping it. Error: {error}", source=source, error=byte_e)
                 continue
             try:
-                if self._is_json_lines:
-                    json_data = [json.loads(line) for line in bytestream.data.splitlines()]
-                else:
-                    json_data = json.loads(bytestream.data)
-
+                json_data = json.loads(bytestream.data)
             except Exception as parse_e:
                 logger.warning(
-                    "Could not deserialize '{source}' as {file_format}. Skipping it. Error: {error}",
-                    source=source,
-                    file_format="JSON Lines" if self._is_json_lines else "JSON",
-                    error=parse_e,
+                    "Could not deserialize '{source}'. Skipping it. Error: {error}", source=source, error=parse_e
                 )
                 continue
             try:
                 jq_result = self._jq_schema.input(json_data)
-                for index, item in enumerate(jq_result):
-                    content = self._get_content(item)
-                    merged_meta = {**bytestream.meta, **metadata, "source": source, "seq_num": index}
-                    new_document = Document(content=content, meta=merged_meta)
-                    documents.append(new_document)
+                for result in jq_result:
+                    content = self._get_content(result)
+                    additional_meta = self._get_additional_metadata(result)
+                    merged_meta = {**bytestream.meta, **metadata, **additional_meta}
+                    documents.append(Document(content=content, meta=merged_meta))
 
             except Exception as convert_e:
                 logger.warning(
@@ -143,3 +138,15 @@ class JSONToDocument:
             return json.dumps(content) if content else ""
         else:
             return str(content) if content is not None else ""
+
+    def _get_additional_metadata(self, record: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Add additional metadata specified by additional_meta_fields argument.
+
+        :param record: Single data payload.
+        :return: A dictionary containing additional metadata fields.
+        """
+        if not self._additional_meta:
+            return {}
+
+        return {field: record.get(field) for field in self._additional_meta}
