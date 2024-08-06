69b11d9920e641a6ea8a22e83bda260b1ff99832
CuriousLearner2 <gautambiswas2004@gmail.com>
2024-08-06 16:17:27 -0700

    Resolved conflict in __init__.py

--- haystack/components/converters/__init__.py
+++ haystack/components/converters/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from haystack.components.converters.azure import AzureOCRDocumentConverter
-from haystack.components.converters.docx import DocxToDocument
+from haystack.components.converters.docx import DOCXMetadata, DOCXToDocument
 from haystack.components.converters.html import HTMLToDocument
 from haystack.components.converters.json import JSONToDocument
 from haystack.components.converters.jq_json import JQToJSON
@@ -11,6 +11,7 @@ from haystack.components.converters.markdown import MarkdownToDocument
 from haystack.components.converters.openapi_functions import OpenAPIServiceToFunctions
 from haystack.components.converters.output_adapter import OutputAdapter
 from haystack.components.converters.pdfminer import PDFMinerToDocument
+from haystack.components.converters.pptx import PPTXToDocument
 from haystack.components.converters.pypdf import PyPDFToDocument
 from haystack.components.converters.tika import TikaDocumentConverter
 from haystack.components.converters.txt import TextFileToDocument
@@ -25,7 +26,9 @@ __all__ = [
     "MarkdownToDocument",
     "OpenAPIServiceToFunctions",
     "OutputAdapter",
-    "DocxToDocument",
+    "DOCXToDocument",
+    "DOCXMetadata",
+    "PPTXToDocument",
     "JSONToDocument",
     "JQToJSON",
 ]
