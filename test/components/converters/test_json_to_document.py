ec16aef1557b373bb988b1aae57078ae6307e451
CuriousLearner2 <gautambiswas2004@gmail.com>
2024-09-27 18:36:26 -0700

    cleanup after hatch

--- test/components/converters/test_json_to_document.py
+++ test/components/converters/test_json_to_document.py
@@ -1,22 +1,20 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-import logging
-from pathlib import Path
 import json
-import pytest
+import logging
 import os
+from pathlib import Path
 
+import pytest
 
 from haystack.components.converters.jq_json import JSONToDocument
 
 
 class TestJSONToDocument:
-    
-  
+    def test_run(self, test_files_path, caplog):
+        """Happy path test for standard and JSON lines JSON"""
 
-
-    def test_run(self, test_files_path,caplog):
         """
         Test if the component runs correctly, both standard JSON and JSON lines
         Using a subset of the nobel prize dataset - https://api.nobelprize.org/v1/prize.json
@@ -24,31 +22,50 @@ class TestJSONToDocument:
         1) Number of documents generated
         2) output[0].content =  .first() value of the jq query - Validate content field
         3) output[0].meta = output of metadata function + additional metadata - Validate metadata
-        
+
         """
-        # **** - Need to do JSON lines
-        # Test data 
+        # Test data
         file_path = test_files_path / "json" / "sample.json"
-        file_path_lines = test_files_path / "json" / "sample_json_lines.json" #json lines
+        file_path_lines = test_files_path / "json" / "sample_json_lines.json"  # json lines
         # file_path.parent.mkdir(parents=True, exist_ok=True)
-        jq_data_schema = '.prizes[].laureates[]?'
-        jq_data_schema_lines = 'del(.share)'
+        jq_data_schema = ".prizes[].laureates[]?"
+        jq_data_schema_lines = "del(.share)"
         expected_content = {
-        "id": "1029", "firstname": "Moungi", "surname": "Bawendi", 
-        "motivation": "\"for the discovery and synthesis of quantum dots\"", 
-        "share": "3"} 
+            "id": "1029",
+            "firstname": "Moungi",
+            "surname": "Bawendi",
+            "motivation": "for the discovery and synthesis of quantum dots",
+            "share": "3",
+        }
         expected_meta = {
-        'dataset': 'nobel data set', 'seq_no': 1, 'file_path': 'sample.json', 
-        'id': '1029', 'firstname': 'Moungi', 'surname': 'Bawendi', 'share': '3','jq_schema': '.prizes[].laureates[]?'}
-        expected_content_lines = {"year": "2023", "category": "chemistry", "id": "1029", "firstname": "Moungi", "surname": "Bawendi", "motivation": "\"for the discovery and synthesis of quantum dots\""}
-        expected_meta_lines =  {'dataset': 'nobel data set', 'id': '1029', 'firstname': 'Moungi', 'surname': 'Bawendi', 
-            'share': None, 'seq_no': 1, 
-            'file_path': 'sample_json_lines.json', 'jq_schema': 'del(.share)'}
-
-
-
-
-    
+            "dataset": "nobel data set",
+            "seq_no": 1,
+            "file_path": "sample.json",
+            "id": "1029",
+            "firstname": "Moungi",
+            "surname": "Bawendi",
+            "share": "3",
+            "jq_schema": ".prizes[].laureates[]?",
+        }
+        expected_content_lines = {
+            "year": "2023",
+            "category": "chemistry",
+            "id": "1029",
+            "firstname": "Moungi",
+            "surname": "Bawendi",
+            "motivation": "for the discovery \
+        and synthesis of quantum dots",
+        }
+        expected_meta_lines = {
+            "dataset": "nobel data set",
+            "id": "1029",
+            "firstname": "Moungi",
+            "surname": "Bawendi",
+            "share": None,
+            "seq_no": 1,
+            "file_path": "sample_json_lines.json",
+            "jq_schema": "del(.share)",
+        }
 
         # Metadata function
         def metadata_func(sample: dict) -> dict:
@@ -61,50 +78,86 @@ class TestJSONToDocument:
 
         converter = JSONToDocument()
         with caplog.at_level(logging.WARNING):
-            output_dict = converter.run(sources = file_path,jq_data_schema=jq_data_schema, metadata_func=metadata_func,
-                                   metadata = {'dataset': 'nobel data set'},json_lines=False)
-        
-        #print ('file path  ', file_path, 'output:   ',output)
-        #print("Current working directory:", os.getcwd())
-        #print("Environment variables:", os.environ)
+            output_dict = converter.run(
+                sources=file_path,
+                jq_data_schema=jq_data_schema,
+                metadata_func=metadata_func,
+                metadata={"dataset": "nobel data set"},
+                json_lines=False,
+            )
+
+        # print ('file path  ', file_path, 'output:   ',output)
+        # print("Current working directory:", os.getcwd())
+        # print("Environment variables:", os.environ)
         output = output_dict["documents"]
         doc_dict = json.loads(output[0].content)
         meta = output[0].meta
         assert len(output) == 25
+        # Normalize for whitespace
+        expected_content = {
+            key: " ".join(value.split()) if isinstance(value, str) else value for key, value in expected_content.items()
+        }
+        doc_dict = {
+            key: " ".join(value.split()) if isinstance(value, str) else value for key, value in doc_dict.items()
+        }
+        expected_meta = {
+            key: " ".join(value.split()) if isinstance(value, str) else value for key, value in expected_meta.items()
+        }
+        meta = {key: " ".join(value.split()) if isinstance(value, str) else value for key, value in meta.items()}
+
+        # Test for equality
+
         assert doc_dict == expected_content
         assert meta == expected_meta
-        
+
         # Test json_lines format
-        
+
         converter_lines = JSONToDocument()
-        
+
         with caplog.at_level(logging.WARNING):
-            output_dict = converter_lines.run(sources = file_path_lines,jq_data_schema=jq_data_schema_lines, metadata_func=metadata_func,
-                                   metadata = {'dataset': 'nobel data set'},json_lines=True)
-        
-        #Look at first element
-        output = output_dict ['documents']
+            output_dict = converter_lines.run(
+                sources=file_path_lines,
+                jq_data_schema=jq_data_schema_lines,
+                metadata_func=metadata_func,
+                metadata={"dataset": "nobel data set"},
+                json_lines=True,
+            )
+
+        # Look at first element
+        output = output_dict["documents"]
         doc_dict = json.loads(output[0][0].content)
         meta = output[0][0].meta
-        
+
         assert len(output) == 25
+
+        # Normalize for whitespace
+        expected_content_lines = {
+            key: " ".join(value.split()) if isinstance(value, str) else value
+            for key, value in expected_content_lines.items()
+        }
+        doc_dict = {
+            key: " ".join(value.split()) if isinstance(value, str) else value for key, value in doc_dict.items()
+        }
+        expected_meta_lines = {
+            key: " ".join(value.split()) if isinstance(value, str) else value
+            for key, value in expected_meta_lines.items()
+        }
+        meta = {key: " ".join(value.split()) if isinstance(value, str) else value for key, value in meta.items()}
+
+        # Test for equality
+
         assert doc_dict == expected_content_lines
         assert meta == expected_meta_lines
-
-        
-        return
 
     def test_run_error_handling(self, test_files_path, caplog):
         """
         Test if the component correctly handles errors.
         """
-        #set up
+        # set up
         file_path_error = "non_existing_file.json"
         converter_error = JSONToDocument()
-
         with caplog.at_level(logging.WARNING):
-            output = converter_error.run(sources = file_path_error,jq_data_schema='', metadata_func=None,
-                                   metadata = None,json_lines=False)
+            output = converter_error.run(  # noqa: F841
+                sources=file_path_error, jq_data_schema="", metadata_func=None, metadata=None, json_lines=False
+            )
         assert "non_existing_file.json" in caplog.text
-        return
-
